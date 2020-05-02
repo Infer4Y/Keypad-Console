89c22e9b6c29076a032d54a32e78065656a9a87b
XavierDD1st <xavierdd1st@textnow.me>
2020-05-01 20:54:02 -0400

    Added more colors. Plan to add more.

--- GameGraphics/Colors.h
+++ GameGraphics/Colors.h
@@ -3,14 +3,20 @@
 
 #include "Arduino.h"
 
-#define BLACK   0x0000
-#define BLUE    0x001F
-#define RED     0xF800
-#define GREEN   0x07E0
-#define CYAN    0x07FF
-#define MAGENTA 0xF81F
-#define YELLOW  0xFFE0
-#define WHITE   0xFFFF
+#define BLACK      0x0000
+#define BLUE       0x001F
+#define DARK_BLUE  0x0029
+#define RED        0xF800
+#define DARK_RED   0x4800
+#define DARK_GREEN 0x0240
+#define GREEN      0x07E0
+#define CYAN       0x07FF
+#define MAGENTA    0xF81F
+#define PURPLE     0x2009
+#define PINK       0xFC5E
+#define YELLOW     0xFFE0
+#define WHITE      0xFFFF
+#define BROWN      0x4900
 
 int16_t getColorFromRef(int id);
 #endif
